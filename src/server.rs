dfe9a1cd3972e0b91511e1fc821b1f9e4f7b9103
squ1dd13 <squ1dd13dev@gmail.com>
2022-03-13 20:08:09 +0000

    Use localhost instead of platform-dependent local IP

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use std::{
     collections::HashMap,
     io::Write,
-    net::{SocketAddr, TcpStream},
+    net::{IpAddr, SocketAddr, TcpStream},
     ops::DerefMut,
 };
 
@@ -11,7 +11,7 @@ use rand::Rng;
 use crate::comm::{CtsMessage, Role, StcMessage, Winner};
 
 pub fn start(port: u16) -> SocketAddr {
-    let addr = SocketAddr::new(local_ip_address::local_ip().unwrap(), port);
+    let addr = SocketAddr::new(IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)), port);
 
     println!("Hosting on {}", addr);
 
